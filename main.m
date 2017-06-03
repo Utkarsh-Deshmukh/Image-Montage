9e929e0f69406ce0b2ee5acef6b915be5638cb85
Utkarsh-Deshmukh <d_utkarsh@yahoo.co.in>
2017-06-03 13:08:13 -0700

    added the output image and changed size of montage images

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clc
 
 bg_dir = 'randomImages';
 coverImg = rgb2gray(imread('minions.jpg'));
-coverImg = imresize(coverImg,5);        % Increasing the resolution of the coverImage
+coverImg = imresize(coverImg,2);        % Increasing the resolution of the coverImage
 L = dir(fullfile(bg_dir,'*.jpg'));
 [rows cols] = size(coverImg);
 
@@ -12,26 +12,36 @@ L = dir(fullfile(bg_dir,'*.jpg'));
 %% This part is to create the averages matrix. If already created, comment this part and load the matrix
 % for(i=1:length(L))
 %     img = imread(fullfile(bg_dir,L(i).name));
+%     img = imresize(img,0.5);
+%     imwrite(img, fullfile(bg_dir,L(i).name));
 %     averages(i) = mean(img(:));
 % end
 
 load averages
 
 %%
-rows = 64*floor(rows/64);               % Montage images are of size 64x64. Hence, making the dimensions of coverImage a factor of 64
-cols = 64*floor(cols/64);
+rows = 32*floor(rows/32);               % Montage images are of size 64x64. Hence, making the dimensions of coverImage a factor of 64
+cols = 32*floor(cols/32);
 
 output = zeros(rows,cols);
-for(i=1:64:rows)
-    for(j=1:64:cols)
-        block = coverImg(i:i+63,j:j+63);
+
+count = 0; maxCount = (rows*cols)/(32*32);
+try
+    delete(h)
+end
+h = waitbar(0,'Initializing waitbar...');
+for(i=1:32:rows)
+    for(j=1:32:cols)
+        count = count + 1;
+        waitbar(count/maxCount,h);
+        block = coverImg(i:i+31,j:j+31);
         blockMean = mean(block(:));
         
         meanDiff = abs(averages - blockMean);
         [val ind] = sort(meanDiff);
-        ind = ind(randi(1));
+        ind = ind(randi(5));
         
-        output(i:i+63,j:j+63) = imread(fullfile(bg_dir,L(ind).name));
+        output(i:i+31,j:j+31) = imread(fullfile(bg_dir,L(ind).name));
     end
 end
 
